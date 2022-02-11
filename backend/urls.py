d5dab0cba2ea6db12dfef9d3c9cf7942b5b9729a
unknown <abdallwario96@gmail.com>
2022-02-12 00:02:01 +0300

    url fix

--- backend/urls.py
+++ backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.contrib.auth.views import LoginView
 from api import views
 from django.views.generic import TemplateView
@@ -24,7 +24,7 @@ urlpatterns = [
     path('api/',include('api.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', views.index),
-    path("", TemplateView.as_view(template_name='index.html'))
+    re_path(".*", TemplateView.as_view(template_name='index.html'))
 
 
 
